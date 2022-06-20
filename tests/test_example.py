bc536bbdad383f2222f990c4de0ffe956758648a
InnokentyVoloshinHibob <innokenty.voloshin@hibob.io>
2022-06-20 11:46:31 +0300

    1
    1

---
+++ tests/test_example.py
@@ -0,0 +1,19 @@
+from seleniumbase import BaseCase
+
+
+class Example(BaseCase):
+
+    @staticmethod
+    def test_example1():
+        if 2+2 != 4:
+            assert False
+
+    @staticmethod
+    def test_example2():
+        if 2 * 2 != 4:
+            assert False
+
+    @staticmethod
+    def test_example3():
+        if 2 / 2 != 1:
+            assert False
